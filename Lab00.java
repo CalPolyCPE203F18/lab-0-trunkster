fbdee7152e9383768de66a35c80c05e7c7a6955b
trunkster <dtrunkey@calpoly.edu>
2018-09-21 16:54:10 -0700

    Final

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,40 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
+public class Lab00 {
+    public static void main(String[] args) {
+        //declaring and initializing some variables
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
 
-   
-   }
+        //printing the variables
+        System.out.println("x:" + " " + x + " " + "y:" + " " + y + " " + "z:" + " " + z);
+
+        //a list (make an array in java)
+        int[] nums = {3, 6, -1, 2};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(nums[i]);
+        }
+
+        //call a function
+        int numFound = char_count(y, 'l');
+        System.out.println("Found:" + numFound);
+
+        //a counting for loop
+        for (int i = 0; i < 11; i++) {
+            System.out.print("" + i + " ");
+
+        }
+
+
+    }
+
+    private static int char_count(String s, char c) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char inString = s.charAt(i);
+            if (inString == c) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
